14c6c41071d06bd484df0c66a0e67cbf4bcc22a3
Joseph Eccles <joeyeccles@gmail.com>
2024-10-22 13:00:51 +0100

    build: config for deply to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,29 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   // Optionally, add any other Next.js config below
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/api-docs",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 const withMDX = mdx({
